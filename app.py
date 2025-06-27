97428e587a304060158f9f816de9c23d4b923a06
Rumit Vasoya <mrrumitvasoya@gmail.com>
2025-06-28 02:14:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def products(category=None):
     return render_template('products.html', products=all_products, categories=categories)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)),debug=True) 
